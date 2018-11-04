7e6345980a87ac34e8c6c7667f000a0d72cc063c
Axel Colson <axel_colson@hotmail.com>
2018-11-04 18:38:42 +0100

    Implemented fragments for the home screen

--- app/src/main/java/be/pxl/mobiledevelopmentproject/MainActivity.java
+++ app/src/main/java/be/pxl/mobiledevelopmentproject/MainActivity.java
@@ -2,19 +2,76 @@ package be.pxl.mobiledevelopmentproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity implements IActionBar {
+
+    private ConstraintLayout constraintLayout;
+
+
+    /**
+     * Clear any existing layout, add the current fragment
+     * to the back stack, and load the new fragment
+     * @param fragment fragment to load
+     */
+
+    public void loadFragment(Fragment fragment){
+        //Clear any existing layout
+        constraintLayout.removeAllViews();
+
+
+        //Load the new fragment to the layout
+        getFragmentManager().beginTransaction()
+                .addToBackStack(null) // Go to the previous fragment when clicking back button
+                .replace(R.id.constraintLayout, fragment)
+                .commit();
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        //clear any existing layouts before popping the stack
+        if (constraintLayout != null){
+            constraintLayout.removeAllViews();
+        }
+
+        //create fragment manager to load previous fragment
+        FragmentManager fragmentManager = getFragmentManager();
+
+        //if there are fragments left in the stack, load the previous fragment
+        // this may be needed when calling addToBackStack(null) to load fragments
+        if (fragmentManager.getBackStackEntryCount() >1){
+            fragmentManager.popBackStack();
+            return;
+        }
+
+        //exit app if there are no more fragments
+        super.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_view);
+
+        //attach the layout to a handle
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+
+        //load the fragment into the layout handle
+        getFragmentManager().beginTransaction()
+                .replace(R.id.constraintLayout, new MainActivityFragment())
+                .commit();
+
 
         getSupportActionBar().hide();
 
